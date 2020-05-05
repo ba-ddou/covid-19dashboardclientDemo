ff13725ef41af5411a669c2a2534e51aa44f9eef
Abdelbasite Badou <abdelbassite.badou@gmail.com>
2020-05-05 18:57:55 +0000

    build 101

---
+++ precache-manifest.747b58f14f535c7717343dc9be11f2ac.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc6ee8e175617ed12b4a8ff16ff2df8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76210eb6573fa5f07410",
+    "url": "/static/css/main.a8197a82.chunk.css"
+  },
+  {
+    "revision": "8676d5cab2f647169071",
+    "url": "/static/js/2.099e7aa9.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.099e7aa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76210eb6573fa5f07410",
+    "url": "/static/js/main.96d03d31.chunk.js"
+  },
+  {
+    "revision": "a2d95c3424e3a583c39f",
+    "url": "/static/js/runtime-main.436818a8.js"
+  },
+  {
+    "revision": "2851584c5d81a014bbab7ceb3feef6c0",
+    "url": "/static/media/Geomanist-Black.2851584c.otf"
+  },
+  {
+    "revision": "e5cd3df26fa3b6db36ab7bf269715f3a",
+    "url": "/static/media/Geomanist-Bold.e5cd3df2.otf"
+  },
+  {
+    "revision": "3cdf858a3452ae0fff879d9d214d238c",
+    "url": "/static/media/Geomanist-Regular.3cdf858a.otf"
+  },
+  {
+    "revision": "14b3a355f612d6181e891efd2c798b5f",
+    "url": "/static/media/Gotham-Black.14b3a355.otf"
+  }
+]);
